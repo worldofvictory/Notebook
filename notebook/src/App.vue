2256d18a52a2a16abbc6e4439a3fea5fd73a402b
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-04-18 11:42:02 +0200

    setup

---
+++ notebook/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button class="">+</button>
+      </header>
+      <div class="card-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
